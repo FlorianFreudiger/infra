7f01cba0e72c5703fcac158d1cb62562b8b29204
Florian Freudiger <25648113+FlorianFreudiger@users.noreply.github.com>
2026-03-08 03:20:49 +0100

    Switch to sudo-rs

--- nix/nixos/core/security.nix
+++ nix/nixos/core/security.nix
@@ -1,9 +1,28 @@
 { ... }:
 {
   flake.nixosModules.security =
-    { ... }:
+    { config, ... }:
     {
       # Ensure firewall is enabled
       networking.firewall.enable = true;
+
+      security = {
+        sudo = {
+          # Harden sudo, only allow members of wheel group to execute sudo
+          execWheelOnly = true;
+
+          # Fall back to sudo if sudo-rs gets disabled somewhere else
+          enable = !config.security.sudo-rs.enable;
+        };
+
+        # Switch to sudo-rs
+        sudo-rs = {
+          enable = true;
+
+          # Copy over relevant sudo options
+          execWheelOnly = config.security.sudo.execWheelOnly;
+          wheelNeedsPassword = config.security.sudo.wheelNeedsPassword;
+        };
+      };
     };
 }
