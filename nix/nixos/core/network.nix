c8e8ea57fc41e0e553175561c67260399a02c242
Florian Freudiger <25648113+FlorianFreudiger@users.noreply.github.com>
2026-03-15 20:31:32 +0100

    Tailscale: disable telemetry, open firewall, use nftables

--- nix/nixos/core/network.nix
+++ nix/nixos/core/network.nix
@@ -1,7 +1,7 @@
 { self, ... }:
 {
   flake.nixosModules.network =
-    { config, ... }:
+    { config, lib, ... }:
     {
       # Use nftables instead of iptables
       networking.nftables.enable = true;
@@ -17,7 +17,12 @@
         extraSetFlags = [
           "--accept-routes"
         ];
+        extraDaemonFlags = [ "--no-logs-no-support" ]; # Disable telemetry
+
+        # Configure network settings to allow direct connections and routing features
+        openFirewall = true;
         useRoutingFeatures = "both";
+
         authKeyFile = config.age.secrets.tailscale-authkey.path;
         authKeyParameters.ephemeral = false;
       };
@@ -25,6 +30,11 @@
       age.secrets.tailscale-authkey = {
         rekeyFile = self + "/secrets/master/tailscale-authkey.age";
       };
+
+      # Use nftables if enabled
+      systemd.services.tailscaled.serviceConfig.Environment = lib.mkIf config.networking.nftables.enable [
+        "TS_DEBUG_FIREWALL_MODE=nftables"
+      ];
 
       # Increase UDP buffer sizes for QUIC
       # See https://github.com/quic-go/quic-go/wiki/UDP-Buffer-Sizes
